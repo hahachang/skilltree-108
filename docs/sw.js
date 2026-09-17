6f9ce378ac12ea931d6a438af2e961c66d9eb386
hahachang <violsonny@gmail.com>
2026-09-17 10:21:44 +0800

    新增泡泡總覽，手機預設檢視

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // 由 parsers/build_pwa.py 產生，請勿手改。
-var CACHE = "skilltree-46c42fb864ac";
+var CACHE = "skilltree-0a61b429908b";
 var ASSETS = ["./", "index.html", "manifest.webmanifest", "graph_math.json", "graph_natural.json", "graph_cross.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
 self.addEventListener("install", function (e) {
