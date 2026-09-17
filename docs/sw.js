b0069561e1c436b7a5a49aaa34a7458a075365b0
hahachang <violsonny@gmail.com>
2026-09-17 08:52:46 +0800

    兩種檢視模式提升為工具列上的正式切換

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // 由 parsers/build_pwa.py 產生，請勿手改。
-var CACHE = "skilltree-9fa4e08ca12b";
+var CACHE = "skilltree-9eb841aecd61";
 var ASSETS = ["./", "index.html", "manifest.webmanifest", "graph_math.json", "graph_natural.json", "graph_cross.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
 self.addEventListener("install", function (e) {
