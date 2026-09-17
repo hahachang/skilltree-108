7524867c5c054c1d232fc783118b6cd7367bc87c
hahachang <violsonny@gmail.com>
2026-09-17 22:25:34 +0800

    泡泡總覽加入年級／學習階段篩選

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // 由 parsers/build_pwa.py 產生，請勿手改。
-var CACHE = "skilltree-0a61b429908b";
+var CACHE = "skilltree-f6e6dc44b068";
 var ASSETS = ["./", "index.html", "manifest.webmanifest", "graph_math.json", "graph_natural.json", "graph_cross.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
 self.addEventListener("install", function (e) {
